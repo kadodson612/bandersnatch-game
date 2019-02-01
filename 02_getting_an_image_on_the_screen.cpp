5614b36cb80acdd5b6035349b7c5cf198c54bef2
kadodson612 <kdodson@nd.edu>
2019-01-31 21:00:53 -0500

    Initial commit

---
+++ 02_getting_an_image_on_the_screen.cpp
@@ -0,0 +1,181 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2019)
+and may not be redistributed without written permission.*/
+
+//Using SDL and standard IO
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include <stdio.h>
+#include <string>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+	
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gXOut = NULL;
+
+
+SDL_Surface* loadSurface( std::string path )
+{
+    //The final optimized image
+    SDL_Surface* optimizedSurface = NULL;
+    
+    //Load image at specified path
+    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
+    if( loadedSurface == NULL )
+    {
+        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
+    }
+    else
+    {
+        //Convert surface to screen format
+        optimizedSurface = SDL_ConvertSurface( loadedSurface, gScreenSurface->format, NULL );
+        if( optimizedSurface == NULL )
+        {
+            printf( "Unable to optimize image %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
+        }
+        
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+    
+    return optimizedSurface;
+}
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Create window
+		gWindow = SDL_CreateWindow( "Bandersnatch", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Get window surface
+			gScreenSurface = SDL_GetWindowSurface( gWindow );
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+    
+    //Load splash image
+    gXOut = loadSurface( "bandersnatch-logo.png" );
+    if( gXOut == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", "bandersnatch-logo.png", SDL_GetError() );
+        success = false;
+    }
+    
+    return success;
+}
+
+void close()
+{
+	//Deallocate surface
+	SDL_FreeSurface( gHelloWorld );
+	gHelloWorld = NULL;
+
+	//Destroy window
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+
+	//Quit SDL subsystems
+	SDL_Quit();
+}
+
+
+int main( int argc, char* args[] )
+{
+	//Start up SDL and create window
+	if( !init() )
+	{
+		printf( "Failed to initialize!\n" );
+	}
+	else
+	{
+		//Load media
+		if( !loadMedia() )
+		{
+			printf( "Failed to load media!\n" );
+		}
+		else
+
+        {
+                //Main loop flag
+                bool quit = false;
+                
+                //Event handler
+                SDL_Event e;
+            
+                //Apply the image
+                SDL_BlitSurface( gXOut, NULL, gScreenSurface, NULL );
+            
+                //Wait two seconds
+                SDL_Delay( 5000 );
+                
+                //While application is running
+                while( !quit )
+                {
+                    //Handle events on queue
+                    while( SDL_PollEvent( &e ) != 0 )
+                    {
+                        //User requests quit
+                        if( e.type == SDL_QUIT )
+                        {
+                            quit = true;
+                        }
+                    }
+                    
+                    //Apply the image
+                    //SDL_BlitSurface( gXOut, NULL, gScreenSurface, NULL );
+                    
+                    //Update the surface
+                    SDL_UpdateWindowSurface( gWindow );
+                }
+        }
+    }
+
+
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
+}
